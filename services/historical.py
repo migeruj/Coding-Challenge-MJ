6fc8816c7445b0f5ca4735824619546d0544d341
migeruj <mjurado850@unab.edu.co>
2024-01-28 21:09:46 -0500

    Add validation accoding with the number of cols + Add chunksize 1000

--- services/historical.py
+++ services/historical.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import APIRouter, HTTPException, UploadFile, File
+from fastapi.responses import JSONResponse
 from models.migration import Table, departments_columns, jobs_columns, employees_columns, schemas
 import pandas as pd
 import awswrangler as wr
@@ -19,40 +20,40 @@ def upload_file(upsert: bool, table_name: Table, include_header: bool = False,ba
     :return:
     Request response
     """
-    try:
-        if batch_file.content_type != 'text/csv':
-            raise HTTPException(status_code=412, detail="File MIME type is not valid.")
+    if batch_file.content_type != 'text/csv':
+        raise HTTPException(status_code=412, detail="File MIME type is not valid.")
 
-        ### Selecting the correct columns names for the table
-        col_names: list[str] = [""]
-        if table_name =='employees':
-            col_names = employees_columns
-        if table_name =='jobs':
-            col_names = jobs_columns
-        if table_name =='departments':
-            col_names = departments_columns
+    ### Selecting the correct columns names for the table
+    col_names: list[str] = [""]
+    if table_name =='employees':
+        col_names = employees_columns
+    if table_name =='jobs':
+        col_names = jobs_columns
+    if table_name =='departments':
+        col_names = departments_columns
 
-        dataframe = pd.read_csv(batch_file.file, header=None, names=col_names,dtype=str, delimiter=',')
+    dataframe: pd.DataFrame = pd.read_csv(batch_file.file, header=None,dtype=str, delimiter=',')
 
-        ### Validation Process
-        cols = dataframe.columns
-        if len(cols)>5:
-            raise HTTPException(status_code=412, detail="More columns than expected")
+    ### Validation Process
+    cols = dataframe.columns
+    if len(cols)>5:
+        raise HTTPException(status_code=412, detail="More columns than expected")
 
-        if table_name == 'employees' and len(cols)!=5:
-            raise HTTPException(status_code=412, detail="Check your batch file, Doesn't correspond to employees schema")
-        if table_name == 'jobs' and len(cols) !=2:
-            raise HTTPException(status_code=412, detail="Check your batch file, Doesn't correspond to jobs schema")
-        if table_name == 'departments' and len(cols) !=2:
-            raise HTTPException(status_code=412, detail="Check your batch file, Doesn't correspond to departments schema")
+    if table_name == 'employees' and len(cols)!=5:
+        raise HTTPException(status_code=412, detail="Check your batch file, Doesn't belong to employees schema")
+    if table_name == 'jobs' and len(cols) !=2:
+        raise HTTPException(status_code=412, detail="Check your batch file, Doesn't belong to jobs schema")
+    if table_name == 'departments' and len(cols) !=2:
+        raise HTTPException(status_code=412, detail="Check your batch file, Doesn't belong to departments schema")
 
-        up_cols = ["id"] if upsert else None
-        res = wr.postgresql.to_sql(df=dataframe,con=pg_conn, table=table_name, schema='public',upsert_conflict_columns=up_cols,
-                                   dtype=schemas.get(table_name), use_column_names=True)
+    dataframe.columns = col_names
 
-        logging.info(res)
+    up_cols: list[str] | None = ["id"] if upsert else None
+    mode: str = 'upsert' if upsert else 'append'
 
-    except Exception as e:
-        logging.error(e)
-    finally:
-        pg_conn.close()
+    table_schema: dict = schemas.get(table_name)
+
+    wr.postgresql.to_sql(df=dataframe,con=pg_conn, table=table_name, schema='public',upsert_conflict_columns=up_cols,
+                               dtype=table_schema, use_column_names=True, mode=mode, chunksize=1000)
+
+    return JSONResponse(status_code=201,content={'message': "Accepted"})
